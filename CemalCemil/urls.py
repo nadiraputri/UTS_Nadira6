863ec66521ea84480ebb97dea0ab5bcf575d2b10
nadirahijriani26 <nadirahijriani26@gmail.com>
2024-05-08 19:24:02 +0700

    update kedua

--- CemalCemil/urls.py
+++ CemalCemil/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("pelanggan/", views.PelangganListCreate.as_view(), name="Pelanggan-views-create")
+    path("pelanggan/", views.PelangganListCreate.as_view(), name="Pelanggan-views-create"),
+    path("pelanggan/<int:pk>/", views.PelangganRetrieveUpdateDestory.as_view(), name="Update"),
 ]
--- CemalCemil/views.py
+++ CemalCemil/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Pelanggan
 from.serializers import PelangganSerializer
 
 class PelangganListCreate(generics.ListCreateAPIView):
     queryset = Pelanggan.objects.all()
     serializer_class = PelangganSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        Pelanggan.objects.all().delete()
+        return Response(status=status.HTTP_204__NO_CONTEN)
+    
+class PelangganRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pelanggan.objects.all()
+    serializer_class = PelangganSerializer
+    lookup_field = "pk"
